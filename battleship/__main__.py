9cb2ea0248c262b4624ed8ad132d303c51870fef
mfrh-tec <165178217+mfrh-tec@users.noreply.github.com>
2024-04-09 10:19:01 -0400

    commitpt3again

--- battleship/__main__.py
+++ battleship/__main__.py
@@ -3,3 +3,14 @@ from board import battleship_board, create_ships
 with open('README.md', 'r') as f:
     print(f.read())
     
+while True:
+    command = input("Enter a command: ")
+    print(command)
+    if command == "hello":
+         print("hello")
+    elif command == "restart":
+         exit()
+    elif command[0] in ["A", "B", "C", ] and int(command[1]) in range(10):
+        print("Shot at", command)
+    else:
+         print("Command not valid")
