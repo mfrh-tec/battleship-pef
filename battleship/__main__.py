4292c298d4e1782e8465dab6a6d4a60a419e8422
mfrh-tec <165178217+mfrh-tec@users.noreply.github.com>
2024-04-16 09:00:05 -0400

    miss and hit

--- battleship/__main__.py
+++ battleship/__main__.py
@@ -1,23 +1,22 @@
 from board import battleship_board, create_ships
 
-abc = ["A", "B", "C", "D", "E", "F", "G", "H", "J"]
-rows_where_ship = int[ "1", "2", "5", "6", "7", "8"]
 with open('README.md', 'r') as f:
     print(f.read())
-    
+
+ship_coordinate = ["B5", "B6", "B7", "D8", "E8", "F8", "G8", "J1", "J2"] 
+abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
 while True:
     command = input("Enter a command: ")
-    print(command)
     if command == "hello":
          print("hello")
     elif command == "restart":
          exit()
     elif command[0] in abc and int(command[1]) in range(10):
         print("Shot at", command)
-        if command[0] in abc[1, 4, 5, 6, 7, 9] and int(command[1]) in rows_where_ship
-        ["B5", "B6", "B7", "D8", "E8", "F8", "G8", "J1", "J2"]
+        if command == ship_coordinate:
             print("THAT'S A HIT!")
-        else
+        else:
             print("Ooh! That's a miss :(")
     else:
          print("Command not valid")
