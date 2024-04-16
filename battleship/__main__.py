88d4791ce40cf3ea0d147bd5b0b1c92cf0e3d872
mfrh-tec <165178217+mfrh-tec@users.noreply.github.com>
2024-04-16 06:47:40 -0400

    trying to establish funcitional coordinates

--- battleship/__main__.py
+++ battleship/__main__.py
@@ -1,5 +1,7 @@
 from board import battleship_board, create_ships
 
+abc = ["A", "B", "C", "D", "E", "F", "G", "H", "J"]
+rows_where_ship = int[ "1", "2", "5", "6", "7", "8"]
 with open('README.md', 'r') as f:
     print(f.read())
     
@@ -10,7 +12,12 @@ while True:
          print("hello")
     elif command == "restart":
          exit()
-    elif command[0] in ["A", "B", "C", ] and int(command[1]) in range(10):
+    elif command[0] in abc and int(command[1]) in range(10):
         print("Shot at", command)
+        if command[0] in abc[1, 4, 5, 6, 7, 9] and int(command[1]) in rows_where_ship
+        ["B5", "B6", "B7", "D8", "E8", "F8", "G8", "J1", "J2"]
+            print("THAT'S A HIT!")
+        else
+            print("Ooh! That's a miss :(")
     else:
          print("Command not valid")
