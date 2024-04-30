09906e1cc9079306561b36b5708c413398fd29a6
mfrh-tec <165178217+mfrh-tec@users.noreply.github.com>
2024-04-30 08:52:21 -0400

    test commit

---
+++ tests/test_actions.py
@@ -0,0 +1,10 @@
+#player 1 attacks 
+def shuffle_ships in range (1,10)
+for shuffle_ships in range (A,J):
+
+coordinate_player1=input("Give your coordinate")
+if coordinate_player1 == (shuffle_ships,shuffle_ships): #not sure it is right 
+ print ("red") 
+ print ("hit") 
+ else if print ("white") 
+print ("miss")
