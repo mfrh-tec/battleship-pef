1765b5cf0943d53c5faf2d6b61cefcd639d247e4
ErikPelusi <165386818+ErikPelusi@users.noreply.github.com>
2024-04-29 21:27:11 -0400

    actions

--- battleship/actions.py
+++ battleship/actions.py
@@ -1,6 +1,3 @@
-def hello():
-    print(42)
-
 #randomize ships in the board 
 def shuffle_ships in range (1,10)
 for shuffle_ships in range (a,j):
@@ -9,14 +6,30 @@ for shuffle_ships in range (a,j):
 coordinate_player1=input("Give your coordinate")
 if coordinate_player1 == (shuffle_ships,shuffle_ships): #not sure it is right 
  print red 
+ print hit 
  else if print white 
-    #i want a code that 
+print miss 
+#change turn
+when coordinate_player1=input("Give your coordinate")
+print end turn 
+#player 2 starts turn 
 #player 2 attacks 
-coordinate_player2=input("Give your coordinate")
+coordinate_player2 = input("Give your coordinate")
 if coordinate_player_2 == shuffle_ships: #not sure it is right 
  print red 
  else if print white 
+#when a boat gets sunken
+if shuffle_ships gets hit 3 times in coordinate_player1 
+ print my ship sunked
+  if shuffle_ships of coordinate_player2 gets hit 3 times 
+ print my ship sunked
 #repeat until winner
+when coordinate_player1 or coordinate_player2 sinks all shuffle_ships
+check if shuffle_ships marked in red 
+print winner 
 #winner
-when any player sink shuffle_ships
-pass winner
+print congrats you won!!
+# ask play again 
+if repeat the game 
+def shuffle_ships in range (1,10)
+for shuffle_ships in range (a,j):
