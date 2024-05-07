d3a13ce1eb4f018324cee0148a6771d426a098f9
pauferro18 <165176899+pauferro18@users.noreply.github.com>
2024-05-07 08:19:33 -0400

    actions

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,5 +1,5 @@
 from battleship.board import new_board
-from battleship.actions import coordinate_to_shoot, verify_if_sink, shuffle_shipss
+from battleship.actions import coordinate_to_shoot, flattened_ships
 
 
 def test_coordinate_to_shoot():
@@ -9,9 +9,14 @@ def test_coordinate_to_shoot():
     assert board[0][0] == "X"
 #make sure that in position 0,0 there is X 
 
-def test_flattened_ships(computers_ships_all):
-    return_ships = []
-    for i in computers_ships_all:
-        for j in i:
-            return_ships.insert(0, j)
-    return return_ships
+
+def test_flattened_ships():
+    board=new_board()
+    flattened_ships(board, 0,0)
+    assert board[0][0] == "X"
+
+
+def test_verify_if_sink ():
+    board=new_board()
+    coordinate_to_shoot(board, 0,0)
+    assert board[0][0] == "X"
