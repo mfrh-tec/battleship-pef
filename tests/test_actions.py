bb33863a7149600e9b1d2561d81aec3e11a5b6a5
mfrh-tec <165178217+mfrh-tec@users.noreply.github.com>
2024-05-06 21:00:54 -0400

    REARRANGING ACTIONS

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -8,3 +8,10 @@ def test_coordinate_to_shoot():
     coordinate_to_shoot(board, 0,0)
     assert board[0][0] == "X"
 #make sure that in position 0,0 there is X 
+
+def test_flattened_ships(computers_ships_all):
+    return_ships = []
+    for i in computers_ships_all:
+        for j in i:
+            return_ships.insert(0, j)
+    return return_ships
